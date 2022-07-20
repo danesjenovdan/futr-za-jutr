d91ded3f8de836a15917156901a36a0064e81760
LepkoQQ <lepko.san@gmail.com>
2022-07-20 11:36:24 +0200

    add components to watch for orientation and size changes

---
+++ src/components/OrientationBanner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="!portrait" class="orientation-banner">
+    Obrni napravo pokonci za najboljšo izkušnjo!
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useWindowSize } from "@vueuse/core";
+
+const { width, height } = useWindowSize();
+
+const portrait = computed(() => width.value < height.value);
+</script>
+
+<style scoped lang="scss">
+.orientation-banner {
+  background: orange;
+  font-size: 1.4rem;
+  font-weight: 700;
+  text-align: center;
+
+  @media (min-width: 992px) and (orientation: landscape) {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/SizeWatcher.vue
@@ -0,0 +1,21 @@
+<template><span></span></template>
+
+<script setup>
+import { onMounted, ref, watchEffect } from "vue";
+import { useElementSize } from "@vueuse/core";
+
+const el = ref(null);
+const { width, height } = useElementSize(el);
+
+onMounted(() => {
+  el.value = document.querySelector("#app");
+});
+
+watchEffect(() => {
+  const size = Math.max(width.value, height.value);
+  const remSize = (10 / 640) * size;
+  console.log(remSize);
+  const root = document.documentElement;
+  root.style.setProperty("--rem-size", `${remSize}px`);
+});
+</script>
