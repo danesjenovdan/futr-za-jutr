1b3c928d0edeb5334328702af3c463c1473f167b
LepkoQQ <lepko.san@gmail.com>
2022-07-28 23:07:22 +0200

    results screen

--- src/components/pages/ResultsScreen.vue
+++ src/components/pages/ResultsScreen.vue
@@ -61,8 +61,34 @@
         </div>
       </div>
     </div>
-    <div>
-      <div>{{ shareState }}</div>
+    <div class="bottom">
+      <div class="bottom-content constrain-width">
+        <div class="title">SESTAVINE</div>
+        <div class="ingredients">
+          <template
+            v-for="(ingredient, ingredientName) in ingredients.ingredients"
+            :key="ingredientName"
+          >
+            <template
+              v-for="(qualityTitle, qualityName) in qualityNames"
+              :key="qualityTitle"
+            >
+              <div class="ingredient">
+                <div class="header">
+                  <div :class="['icon', qualityName]">
+                    <img :src="ingredient[qualityName].icon" />
+                  </div>
+                  <div class="info">{{ ingredient[qualityName].name }}</div>
+                </div>
+                <div class="description">
+                  {{ ingredient[qualityName].long_description }}
+                </div>
+              </div>
+              <div class="divider"></div>
+            </template>
+          </template>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -140,7 +166,7 @@ const qualityCounts = computed(() => {
   background-color: $color-dark-1;
 
   .top-content {
-    padding: 1.1rem 1.6rem;
+    padding: 1.1rem 1.6rem 4rem 1.6rem;
 
     .logo {
       display: block;
@@ -283,6 +309,80 @@ const qualityCounts = computed(() => {
       font-style: italic;
       line-height: 1;
       color: $color-accent-primary-contrast;
+    }
+  }
+}
+
+.bottom {
+  background: $color-light;
+
+  .bottom-content {
+    padding: 4rem 2rem 1.1rem 2rem;
+
+    .title {
+      color: $color-dark-1;
+      font-size: 1.8rem;
+      font-weight: 800;
+      font-style: italic;
+      text-align: center;
+    }
+
+    .ingredients {
+      .ingredient {
+        margin: 2.4rem 0;
+
+        .header {
+          display: flex;
+
+          .icon {
+            width: 9.4rem;
+            height: 9.6rem;
+            margin-right: 1.7rem;
+            padding: 1.3rem 1.2rem;
+            background-color: transparent;
+            background-image: url("../../assets/images/backgrounds/okvir-moder.svg");
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+
+            &.best {
+              background-image: url("../../assets/images/backgrounds/okvir-zelen.svg");
+            }
+
+            &.worst {
+              background-image: url("../../assets/images/backgrounds/okvir-rdec.svg");
+            }
+
+            img {
+              width: 100%;
+              height: 100%;
+              object-fit: contain;
+            }
+          }
+
+          .info {
+            color: $color-dark-1;
+            font-size: 1.5rem;
+            font-weight: 600;
+            font-style: italic;
+          }
+        }
+
+        .description {
+          margin-top: 2rem;
+          color: $color-dark-1;
+          font-size: 1.2rem;
+          font-weight: 300;
+        }
+      }
+
+      .divider {
+        height: 0.9rem;
+        background-color: transparent;
+        background-image: url("../../assets/images/backgrounds/divider-moder.svg");
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center center;
+      }
     }
   }
 }
