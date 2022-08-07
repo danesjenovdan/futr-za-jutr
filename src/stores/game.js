097c41a3a36675ca84377e1ba66ada0f9bedee09
LepkoQQ <lepko.san@gmail.com>
2022-08-07 16:17:33 +0200

    move on to next order only after failed sound and text

--- src/stores/game.js
+++ src/stores/game.js
@@ -158,6 +158,9 @@ export const useGameStore = defineStore("gameStore", {
       if (state.ingredientSelectorOpen && !state.ingredientSelection) {
         return true;
       }
+      if (state.currentOrderFailed) {
+        return true;
+      }
       return false;
     },
   },
@@ -174,9 +177,11 @@ export const useGameStore = defineStore("gameStore", {
         window.gameAudio.music.play();
       }
 
+      let failedOrderCleared = false;
       if (this.currentOrderFailed) {
         if (now - this.currentOrderFailed > 2000) {
           this.currentOrderFailed = null;
+          failedOrderCleared = true;
         }
       }
 
@@ -216,7 +221,6 @@ export const useGameStore = defineStore("gameStore", {
 
       const lastOrder = last(this.orderQueue);
       if (!lastOrder || now - lastOrder.createdAt > this.orderDelay) {
-        this.orderQueue.push(createNewOrder(undefined, now, this.orderTime));
         this.orderDelay = Math.max(
           ORDER_DELAY_MIN_MS,
           this.orderDelay - ORDER_DELAY_SUBTRACT_MS
@@ -225,12 +229,15 @@ export const useGameStore = defineStore("gameStore", {
           ORDER_TIME_MIN_MS,
           this.orderTime - ORDER_TIME_SUBTRACT_MS
         );
+        this.orderQueue.push(createNewOrder(undefined, now, this.orderTime));
       }
 
       // current food failed, start new one
       if (this.currentOrderFailed === now) {
         this.ingredientSelectorOpen = false;
         this.ingredientSelection = null;
+      }
+      if (failedOrderCleared) {
         this.combinedFoods.pop();
         const nextFood = createNewFood(this.currentOrder.id);
         this.combinedFoods.push(nextFood);
